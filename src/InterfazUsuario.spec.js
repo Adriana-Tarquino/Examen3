6c2274226fcee9227a0e97015a7bbb22ff015013
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-05-31 11:44:48 -0400

    Deberia mostra venta

--- src/InterfazUsuario.spec.js
+++ src/InterfazUsuario.spec.js
@@ -9,6 +9,18 @@ describe("Pruebas", () => {
         const resultado = document.querySelector("#titulo");
         expect(resultado.innerHTML).toEqual("VENTA");
     });
-     
+    it("Deberia mostrar monto venta", () => {
+        const resultado = document.querySelector("#monto-venta");
+        expect(resultado.innerHTML).toEqual("Monto venta");
+    });
+    it("Deberia mostrar el monto de la venta", () => {
+        const monto = document.querySelector("#monto-venta");
+        monto.value = "50";
+    
+        const botonAdivinar = document.querySelector("#boton-adivinar");
+        botonAdivinar.click();
+       // const resultado = document.querySelector("#resultado-div");
+        //expect(resultado.innerHTML).toEqual("La palabra es arbol");
+    }); 
 
 });
